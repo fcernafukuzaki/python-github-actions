90f0c73e54934009b0c6c245d015489443620aa4
Francisco Cerna Fukuzaki <fcerna.ai@gmail.com>
2024-02-28 11:37:05 -0500

    Hot fix: Validar versión de Python.

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import sys
 
 
 def test_python_version():
-    version = float(f'{sys.version_info.major}.{sys.version_info.minor}')
+    version = f'{sys.version_info.major}.{sys.version_info.minor}'
     print(f'Versión de Python: {version}')
 
     assert version >= "3.9", 'Se está usando una versión inferior a Python 3.9.'
