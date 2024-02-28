1f5ab7d66973c5e47a8c6f6d9e3c810b2f2ca20c
Francisco Cerna Fukuzaki <fcerna.ai@gmail.com>
2024-02-28 11:30:19 -0500

    Test: Validar versión de Python.

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+import sys
+
+
+def test_python_version():
+    version = float(f'{sys.version_info.major}.{sys.version_info.minor}')
+    print(f'Versión de Python: {version}')
+
+    assert version >= 3.9, 'Se está usando una versión inferior a Python 3.9.'
